105ecf56bb9f8e3df691a0e1b67910fcc21d82ea
TYSS <TYSS@TYSS-4F-PJT-034>
2017-11-15 15:57:21 +0530

    init

---
+++ src/jenkis/testJenkis.java
@@ -0,0 +1,13 @@
+package jenkis;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testJenkis {
+	
+	@Test
+	public void testJenk(){
+		Reporter.log("Testing jenl=kin",true);
+	}
+
+}
